dc0600788c30790c9074a9f21f556cf0ff03d8d9
Sharon <sharon.wang16@hotmail.com>
2017-03-25 15:51:22 -0600

    moved memory hashmap from DSM to LocalMemory

--- src/LocalMemory.java
+++ src/LocalMemory.java
@@ -1,13 +1,52 @@
 /**
  * Created by Sharon on 3/24/2017.
  */
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class LocalMemory {
-    public String load(String x){
-        // TODO: return the value of x read from memory
-        return x;
+    ConcurrentHashMap<String, Integer> memory = new ConcurrentHashMap(20);
+
+    public LocalMemory(){
+        memory.put("flag_1",-2);
+        memory.put("flag_2",-2);
+        memory.put("flag_3",-2);
+        memory.put("flag_4",-2);
+        memory.put("flag_5",-2);
+        memory.put("flag_6",-2);
+        memory.put("flag_7",-2);
+        memory.put("flag_8",-2);
+        memory.put("flag_9",-2);
+        memory.put("flag_10", -2);
+        memory.put("turn_1",0);
+        memory.put("turn_2",0);
+        memory.put("turn_3",0);
+        memory.put("turn_4",0);
+        memory.put("turn_5",0);
+        memory.put("turn_6",0);
+        memory.put("turn_7",0);
+        memory.put("turn_8",0);
+        memory.put("turn_9",0);
+        memory.put("turn_10",0);
     }
 
-    public void store(String x, String v){
-        // TODO: stores the value v of x to memory
+    // return the value of x read from memory
+    public int load(String x){
+        if (memory.contains(x)) {
+            return memory.get(x);
+        }
+        else{
+            System.out.println("cannot find "+ x +" ");
+            return -5;
+        }
+    }
+
+    // stores the value v of x to memory
+    public void store(String x, int v){
+        if(memory.contains(x)){
+            memory.replace(x,v);
+        }else{
+            memory.put(x,v);
+        }
     }
 }
